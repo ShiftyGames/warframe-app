d1f12d7f0414ae43add8f626c9df4478d606de52
Shifty Games <ShiftyGames.dev@gmail.com>
2025-06-14 14:38:41 -0500

    fix void_trader SentinelPrecept classification and fetch accurate Item Names

--- src/gen_void_trader.js
+++ src/gen_void_trader.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { get, WF_API } from './utils.js';
+import { wfapi_items_by_uniqueName } from './wfapi/item-search.js';
 
 /////////
 /// main
@@ -13,7 +14,8 @@ var data_void_trader = {
 };
 
 const weapon_prefix = '/Lotus/StoreItems/Weapons';
-const companion_prefix = '/Lotus/StoreItems/Types/Sentinels';
+const companion_mod_prefix = '/Lotus/StoreItems/Types/Sentinels/SentinelPrecepts';
+const companion_prefix = '/Lotus/StoreItems/Types/Sentinels/';
 const mod_prefix = '/Lotus/StoreItems/Upgrades/Mods';
 const vehicle_prefix = '/Lotus/StoreItems/???';
 
@@ -21,15 +23,35 @@ for (let offer of trader['inventory']) {
     var offer_type = '';
     if (offer.uniqueName.startsWith(weapon_prefix)) {
         offer_type = 'weapon';
+    } else if (offer.uniqueName.startsWith(companion_mod_prefix)) {
+        offer_type = 'mod';
     } else if (offer.uniqueName.startsWith(companion_prefix)) {
         offer_type = 'companion';
     } else if (offer.uniqueName.startsWith(mod_prefix)) {
         offer_type = 'mod';
     }
     if (offer_type != '') {
+        const item_uniqueName = offer.uniqueName.replace(
+            '/Lotus/StoreItems',
+            '/Lotus',
+        );
+        const item_matches = await wfapi_items_by_uniqueName(offer.uniqueName);
+        var item = null;
+        console.log('lookingfor: ' + item_uniqueName);
+        for (let itm of item_matches) {
+            console.log('  checking: ' + itm.uniqueName);
+            if (itm.uniqueName == item_uniqueName) {
+                item = itm;
+            }
+        }
+
+        if (item == null) {
+            console.log('probably a bug here');
+            item = item_matches[0];
+        }
         const data_offer = {
-            item: offer.item,
-            id: offer.item.toLowerCase().split(' ').join('_'),
+            item: item.name,
+            id: item.name.toLowerCase().split(' ').join('_'),
             type: offer_type,
             ducats: offer.ducats,
             credits: offer.credits,
--- src/wfapi/item-search.js
+++ src/wfapi/item-search.js
@@ -10,6 +10,12 @@ export function wfapi_item_by_uniqueName(item_uniqueName, opts) {
     return get(WF_API + "/items/" + end_of_url, opts);
 }
 
+export function wfapi_items_by_uniqueName(item_uniqueName, opts) {
+    const item = item_uniqueName.split("/").pop();
+    const end_of_url =  encodeURIComponent(item) + "/?by=uniqueName"
+    return get(WF_API + "/items/search/" + end_of_url, opts);
+}
+
 export function wfapi_item_search(item, opts) {
     return get(WF_API + "/items/search/" + encodeURIComponent(item) + "/", opts);
 }
